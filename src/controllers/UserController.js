658ef02651b70f6a1792d41880d8e4cfe4e7e3fd
Hoang Loc <hoangloc1403@gmail.com>
2023-12-02 16:00:20 +0700

    update a

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -168,7 +168,7 @@ const refreshToken = async (req, res) => {
 const logoutUser = async (req, res) => {
   try {
     res.clearCookies("refresh_token");
-    return res.status(400).json({
+    return res.status(200).json({
       status: "OK",
       message: "Logout successfully",
     });
